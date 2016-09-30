9df7ffa0a193ee3c73e16e8f231378acc13c4d35
Karthik Kannan <karthikk.314@gmail.com>
2016-09-30 19:08:39 +0530

    Create Post model, migrate it and also enable it in the admin backend.
    Create Post model with Description,Title, Image, Location. Migrate the
    model and make it available in the admin backend panel.
    TODO: Allow multiple image uploads and also create filters in the
    backend. Show posts.

--- ProjCostumeParty/main/models.py
+++ ProjCostumeParty/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=350)
+    slug = models.SlugField(max_length=350,unique_for_date='publish')
+    description = models.TextField(default='Hello World')
+    location = models.CharField(max_length=250,default='Bangalore')
+    images = models.ImageField(upload_to='uploads/')
+    publish = models.DateTimeField(default=timezone.now)
+
+
+class Meta:
+    ordering = ('-publish',)
+
+def __str__(self):
+    self.title
