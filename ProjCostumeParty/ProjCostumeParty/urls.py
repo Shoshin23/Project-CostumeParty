7b72cac887865667e6242ae50472b29f5e248ba7
Karthik Kannan <karthikk.314@gmail.com>
2016-10-08 23:07:11 +0530

    Add proper media URLs and update urls.py with associated url patterns.
    Add proper media URLs in settings file and also add new url patterns to
    recognise these. TODO: Style the info window. Spruce it up.

--- ProjCostumeParty/ProjCostumeParty/urls.py
+++ ProjCostumeParty/ProjCostumeParty/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
-
-
+from django.contrib.staticfiles.urls import static,staticfiles_urlpatterns
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('main.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
