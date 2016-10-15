9ea2a1a8233431ce84a56ceca9e0e6da17a952b9
Karthik Kannan <karthikk.314@gmail.com>
2016-10-16 02:41:10 +0530

    Fix naming issues in settings.py file.

--- ProjCostumeParty/ProjCostumeParty/settings.py
+++ ProjCostumeParty/ProjCostumeParty/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 MEDIA_ROOT = '/Users/karthikkannan/Project-CostumeParty/ProjCostumeParty/uploads'
 MEDIA_URL = '/uploads/'
